a7ba47ea302a17a7248d51ebfb14a38941b343a4
Roger Rajaratnam <roger@sourcier.uk>
2017-07-05 14:53:51 +0100

    Add admin routes

---
+++ express/routes/admin-routes.js
@@ -0,0 +1,25 @@
+module.exports = function() {
+
+    function start({ manifest = {}, app }, cb) {
+
+        app.get('/__/manifest', (req, res) => res.json(manifest));
+
+        app.post('/__/error', (req, res) => {
+            setTimeout(() => process.emit('error', new Error('On Noes')))
+        });
+
+        app.post('/__/crash', (req, res) => {
+            setTimeout(() => undefined.meh)
+        });
+
+		app.post('/__/reject', (req, res) => {
+            setTimeout(() => Promise.reject(new Error('Oh Noes')))
+        });
+
+        cb()
+    }
+
+    return {
+        start: start
+    }
+};
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const readLoggedInUserFromCookie = require('./login/readLoggedInUserFromCookie');
 const writeLoggedInUserToCookie = require('./login/writeLoggedInUserToCookie');
 const logOutUser = require('./login/logOutUser');
+const AdminRoutes = require("./express/routes/admin-routes");
 
 module.exports = {
-  readLoggedInUserFromCookie,
-  writeLoggedInUserToCookie,
-  logOutUser
+	readLoggedInUserFromCookie,
+	writeLoggedInUserToCookie,
+	logOutUser,
+	AdminRoutes
 };
