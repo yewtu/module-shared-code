4f55a4b94cd19c89c38e94618b37c0bba447ee83
balazs-horvath <balazs.horvath@jaystack.com>
2017-07-19 15:48:15 +0200

    Added stages resources.js

--- config/resources.js
+++ config/resources.js
@@ -1,31 +1,29 @@
 module.exports = {
-	resources: {
-		[
+	resources: [
+		{
+			name: "service-marketplace",
+			URI: {
+				local: "http://0.0.0.0:3004"
+			}
+		},
+		{
+			name: "app-marketplace",
+			URI: { 
+				local: "http://0.0.0.0:3002" 
+			}
+		},
+		{
+			name: "app-shopfront",
+			URI: {
+				local: "http://0.0.0.0:3003"
+			},
+		},
+		{
+			name: "service-b2c-store",
+			URI: {
+				local: "http://0.0.0.0:3001"
+			}	
+		},
+	]
 
-			{
-				name: "service-marketplace",
-				URI: {
-					local: "http://0.0.0.0:3004"
-				}
-			},
-			{
-				name: "app-marketplace",
-				URI: { 
-					local: "http://0.0.0.0:3002" 
-				}
-			},
-			{
-				name: "app-shopfront",
-				URI: {
-					local: "http://0.0.0.0:3003"
-				},
-			},
-			{
-				name: "service-b2c-store",
-				URI: {
-					local: "http://0.0.0.0:3001"
-				}	
-			},
-		]
-	}
 }
