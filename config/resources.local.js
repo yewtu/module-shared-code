acf3e540de23c4ef45071ea02ca1e4840e700986
balazs-horvath <balazs.horvath@jaystack.com>
2017-07-19 16:24:37 +0200

    Added config for different deployment stages

---
+++ config/resources.local.js
@@ -0,0 +1,24 @@
+module.exports = {
+    resources: [
+        {
+            name: "service-marketplace",
+            URI: "http://0.0.0.0:3004"
+
+        },
+        {
+            name: "app-marketplace",
+            URI: "http://0.0.0.0:3002"
+
+        },
+        {
+            name: "app-shopfront",
+            URI: "http://0.0.0.0:3003"
+
+        },
+        {
+            name: "service-b2c-store",
+            URI: "http://0.0.0.0:3001"
+
+        },
+    ]
+};
