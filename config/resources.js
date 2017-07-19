5a3fa74aaa62d343150be1d3a111d880d24b0eba
balazs-horvath <balazs.horvath@jaystack.com>
2017-07-19 10:08:24 +0200

    Added resources.js

---
+++ config/resources.js
@@ -0,0 +1,8 @@
+module.exports = {
+    resources: {
+	    service-marketplace : 0.0.0.0:3004,
+	    app-marketplace: 0.0.0.0:3002,
+	    app-shopfront: 0.0.0.0:3003,
+	    service-b2c-store: 0.0.0.0:3001
+    }
+}
