73d795c91901b4c2d870b630227e309d4bc7a3bf
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-07-27 15:04:05 +0100

    PM2 config to run all marketplace apps / services

---
+++ pm2/pm2-marketplace.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	apps: [
+		{
+			name: "app-b2b-marketplace",
+			script: "./app-b2b-marketplace/dist/server/index.js",
+			watch: true,
+			cwd: "./app-b2b-marketplace",
+			env: {
+				"SERVICE_ENV": "local",
+				"NODE_ENV": "development"
+			}
+		},
+		{
+			name: "service-b2b-marketplace",
+			script: "./service-b2b-marketplace/index.js",
+			cwd: "./service-b2b-marketplace",
+			watch: true,
+			node_args: ["--harmony"],
+			env: {
+				"SERVICE_ENV": "local",
+				"NODE_ENV": "development"
+			}
+		},
+		{
+			name: "app-cow-publisher",
+			script: "./app-cow-publisher/dist/server/index.js",
+			cwd: "./app-cow-publisher",
+			watch: true,
+			env: {
+				"SERVICE_ENV": "local",
+				"NODE_ENV": "development"
+			}
+		}
+	]
+}
