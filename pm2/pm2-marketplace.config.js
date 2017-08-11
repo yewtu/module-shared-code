f5df3dc9d93e69ea5bd19c28d972a5c7a6cb2f67
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-08-11 18:21:55 +0100

    Add b2c store, shopfront apps to PM2 config

--- pm2/pm2-marketplace.config.js
+++ pm2/pm2-marketplace.config.js
@@ -4,16 +4,6 @@ Copy this file to the folder one level above the repos, then run with 'pm2 start
 module.exports = {
 	apps: [
 		{
-			name: "app-b2b-marketplace",
-			script: "./app-b2b-marketplace/dist/server/index.js",
-			watch: true,
-			cwd: "./app-b2b-marketplace",
-			env: {
-				"SERVICE_ENV": "local",
-				"NODE_ENV": "development"
-			}
-		},
-		{
 			name: "service-b2b-marketplace",
 			script: "./service-b2b-marketplace/index.js",
 			cwd: "./service-b2b-marketplace",
@@ -28,6 +18,47 @@ module.exports = {
 			name: "app-cow-publisher",
 			script: "./app-cow-publisher/dist/server/index.js",
 			cwd: "./app-cow-publisher",
+			watch: true,
+			env: {
+				"SERVICE_ENV": "local",
+				"NODE_ENV": "development"
+			}
+		},
+		{
+			name: "app-b2b-marketplace",
+			script: "./app-b2b-marketplace/dist/server/index.js",
+			watch: true,
+			cwd: "./app-b2b-marketplace",
+			env: {
+				"SERVICE_ENV": "local",
+				"NODE_ENV": "development"
+			}
+		},
+		{
+			name: "service-b2c-store",
+			script: "./service-b2c-store/index.js",
+			cwd: "./service-b2c-store",
+			watch: true,
+			node_args: ["--harmony"],
+			env: {
+				"SERVICE_ENV": "local",
+				"NODE_ENV": "development"
+			}
+		},
+		{
+			name: "app-b2c-store",
+			script: "./app-b2c-store/dist/server/index.js",
+			cwd: "./app-b2c-store",
+			watch: true,
+			env: {
+				"SERVICE_ENV": "local",
+				"NODE_ENV": "development"
+			}
+		},
+		{
+			name: "app-b2c-shopfront",
+			script: "./app-b2c-shopfront/dist/server/index.js",
+			cwd: "./app-b2c-shopfront",
 			watch: true,
 			env: {
 				"SERVICE_ENV": "local",
