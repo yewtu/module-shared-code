003321b8ccc9f4103ca4460f119f8dffc022a09e
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-08-14 13:34:16 +0100

    Add certification types data

---
+++ provenance/certificationTypes.js
@@ -0,0 +1,14 @@
+module.exports = {
+	grassFed: {
+		label: 'Grass Fed'
+	},
+	organic: {
+		label: 'Organic'
+	},
+	hormoneFree: {
+		label: 'Hormone Free'
+	},
+	redTractor: {
+		label: 'Red Tractor'
+	}
+};
