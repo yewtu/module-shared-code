a827544cedb92daaf0716e39c09f4bdcae8bb1dd
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-09-04 09:28:52 +0100

    Login code - add body-parser and cookie-parser as dependencies

--- login/appLoginRoutes.js
+++ login/appLoginRoutes.js
@@ -1,8 +1,14 @@
 const readLoggedInUserFromCookie = require('./helpers/readLoggedInUserFromRequest');
 const writeLoggedInUserToCookie = require('./helpers/writeLoggedInUserToCookie');
 const logoutUser = require('./helpers/logoutUser');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 module.exports = (app, express) => {
+	app.use(cookieParser());
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
 	app.use(readLoggedInUserFromCookie);
 	app.post('/login', writeLoggedInUserToCookie);
 	app.get('/logout', logoutUser);
