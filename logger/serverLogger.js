bbc7415b4989a2b389d1e15e32d2beb9e0139771
balazs-horvath <balazs.horvath@jaystack.com>
2017-09-06 13:40:42 +0200

    Simplified the logMiddleWare

--- logger/serverLogger.js
+++ logger/serverLogger.js
@@ -2,7 +2,12 @@ const chalk = require("chalk");
 const hogan = require("hogan.js");
 const prepper = require("prepper");
 
-module.exports = () => {
+/**
+ * The module exports a function that creates the logger and some logging utility functions.
+ * @param details
+ * @return {{logger, handleLogRoute: (function(*, *)), logMiddleWare: (function(*, *, *)), listenToGlobalUncaughtErrors: (function())}}
+ */
+module.exports = ({details=false}) => {
 
     const colours = {
         debug: chalk.gray,
@@ -12,18 +17,20 @@ module.exports = () => {
         default: chalk.white
     };
 
-    const response = hogan.compile(`{{{messageSource}}} - {{{timestamp}}} - {{{displayTracer}}} {{{displayLevel}}} {{package.name}} {{{request.method}}} {{{response.statusCode}}} {{{request.url}}}`);
-    const error = hogan.compile(`{{{messageSource}}} - {{{timestamp}}} - {{{displayTracer}}} {{{displayLevel}}} {{package.name}} {{{message}}} {{{code}}}\n{{{error.stack}}}`);
-    const info = hogan.compile(`{{{messageSource}}} - {{{timestamp}}} - {{{displayTracer}}} {{{displayLevel}}} {{package.name}} {{{message}}} {{{details}}}`);
+    const response = hogan.compile(`{{{messageSource}}}  {{{displayTracer}}} {{{displayLevel}}} - {{{timestamp}}} -  {{{method}}} {{{url}}} {{package.name}} {{{request.method}}} {{{response.statusCode}}} {{{request.url}}}`);
+    const error = hogan.compile(`{{{messageSource}}}  {{{displayTracer}}} {{{displayLevel}}} - {{{timestamp}}} -  {{{method}}} {{{url}}} {{package.name}} {{{message}}} {{{code}}}\n{{{error.stack}}}`);
+    const info = hogan.compile(`{{{messageSource}}}  {{{displayTracer}}} {{{displayLevel}}} - {{{timestamp}}} -  {{{method}}} {{{url}}} {{package.name}} {{{message}}} {{{${details?"details":""}}}}`);
 
     const onMessage = (event) => {
         const timestamp = (new Date(event.timestamp || Date.now) ).toLocaleString();
         const data = Object.assign({}, event, {
-            messageSource: event.messageSource || "server",
+            messageSource: event.messageSource && typeof event.messageSource === "string" ? event.messageSource.toUpperCase() : "SERVER",
             displayTracer: "tracer" in event ? event.tracer.substr(0, 6) : '------',
             displayLevel: event.level.toUpperCase(),
             timestamp,
-            details: Object.keys(event).length ? `\n ${JSON.stringify(event, null, 2)}` : ''
+            details: Object.keys(event).length ? `\n ${JSON.stringify(event, null, 2)}` : '',
+            method: event.method || '',
+            url: event.url || '',
         });
         const colour = colours[event.level] || colours.default;
         const log = console[event.level] || console.info; // eslint-disable-line no-console
@@ -37,7 +44,7 @@ module.exports = () => {
 
     const logger = new prepper.Logger({ handlers: [
         new handlers.Merge({ package: pkg }),
-        // new handlers.Merge({ someAddedInfo: "Plus info"}),
+        // TODO: what else should be in the detailed logging?
         // new handlers.Merge({ service: { env: process.env.SERVICE_ENV } }),
         // new handlers.Process(),
         new handlers.System(),
@@ -48,33 +55,23 @@ module.exports = () => {
 
     return {
         logger,
-        //TODO: show in documentations that this function needs express bodyparser.json()
-        handleLogRequte: ( req, res ) => {
-            const { event } = req;
+        handleLogRoute: ( req, res ) => {
+            const { event } = req.body;
             if(!event) {
-                //TODO: should check event schema with Joi
                 res.status(400).send();
                 return;
             }
             onMessage(event);
+            res.status(200).send();
         },
         logMiddleWare: (req, res, next) => {
-            const {url,method,headers,parms} = req;
-            const logger = req.app.locals.logger.child({ handlers: [
-                new handlers.Tracer(),
-                new handlers.Merge(pick(req, ['url', 'method', 'headers', 'params']), { key: 'request' })
-            ]});
-
-            onHeaders(res, () => {
-                const response = { response: { statusCode: res.statusCode, headers: res.headers } };
-                if (res.statusCode === 400) logger.error(req.url, response);
-                if (res.statusCode < 500) logger.info(req.url, response);
-                else logger.error(req.url, response);
-            });
+            const {url,method,headers,params,body={}} = req;
+            logger.log({url, method, headers, params, body});
+            next();
         },
         listenToGlobalUncaughtErrors: () => {
             process.on('uncaughtException', function ( err ) {
-                console.log(err.stack);
+                logger.error(err);
                 process.exit();
             });
         }
