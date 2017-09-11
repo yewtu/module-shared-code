620ee5b3b3c9dff937e5bf24b1fa9a505af52b8f
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-09-11 09:30:36 +0100

    getUserProfile - No need to return promise from async function

--- login/helpers/getUserProfile.js
+++ login/helpers/getUserProfile.js
@@ -1,8 +1,6 @@
 const userProfiles = require('./userProfiles.json');
 
 module.exports = async userName => {
-	return new Promise((resolve, reject) => {
-		if (userProfiles[userName]) return resolve(userProfiles[userName]);
-		reject(new Error(`User ${userName} not found`));
-	});
+	if (userProfiles[userName]) return resolve(userProfiles[userName]);
+	throw new Error(`User ${userName} not found`);
 };
