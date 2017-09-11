1a5259d9a6612215d92277f74671786f1c2c8806
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-09-11 09:48:21 +0100

    getUserProfile - remove resolve

--- login/helpers/getUserProfile.js
+++ login/helpers/getUserProfile.js
@@ -1,6 +1,6 @@
 const userProfiles = require('./userProfiles.json');
 
 module.exports = async userName => {
-	if (userProfiles[userName]) return resolve(userProfiles[userName]);
+	if (userProfiles[userName]) return userProfiles[userName];
 	throw new Error(`User ${userName} not found`);
 };
