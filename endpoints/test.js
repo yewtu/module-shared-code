2b8271eddf921d3e1b3d9965382bd48e338bced1
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-09-25 16:19:25 +0100

    Add test endpoints

--- endpoints/test.js
+++ endpoints/test.js
@@ -1,4 +1,63 @@
 module.exports = {
-    resources: [
-    ]
+	resources: [
+		{
+			name: "app-b2c-store",
+			URI: "http://app-b2c-store-test.herokuapp.com"
+
+		},
+		{
+			name: "service-marketplace",
+			URI: "https://service-marketplace-test.herokuapp.com"
+
+		},
+		{
+			name: "app-marketplace",
+			URI: "http://app-marketplace-test.herokuapp.com"
+
+		},
+		{
+			name: "app-shopfront",
+			URI: "http://app-b2c-shopfront-test.herokuapp.com"
+
+		},
+		{
+			name: "service-b2c-store",
+			URI: "http://service-b2c-store-test.herokuapp.com"
+
+		},
+		{
+			name: "app-cow-publisher",
+			URI: "http://app-cow-publisher-test.herokuapp.com"
+
+		},
+		{
+			name: "app-b2b-marketplace",
+			URI: "http://app-b2b-marketplace-test.herokuapp.com"
+
+		},
+		{
+			name: "service-b2b-marketplace",
+			URI: "http://service-b2b-market-test.herokuapp.com"
+		},
+		{
+			name: "app-imploder",
+			URI: "https://yewtu-imploder-test.herokuapp.com"
+		},
+		{
+			name: "service-imploder",
+			URI: "https://service-imploder-test.herokuapp.com"
+		},
+		{
+			name: "service-optimizer",
+			URI: "https://service-optimizer.herokuapp.com"
+		},
+		{
+			name: "service-farm",
+			URI: "https://service-farm.herokuapp.com"
+		},
+		{
+			name: "app-farm",
+			URI: "https://app-farm.herokuapp.com"
+		}
+	]
 };
